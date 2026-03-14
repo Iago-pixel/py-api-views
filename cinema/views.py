ac6b3afbbd4db143f0e3ed7aa04b667dc610f661
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2026-03-14 17:25:44 -0300

    Implement patch method in GenreDetail for partial updates

--- cinema/views.py
+++ cinema/views.py
@@ -64,7 +64,7 @@ class GenreDetail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
     def patch(self, request, pk):
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre, data=request.data, partial=True)
